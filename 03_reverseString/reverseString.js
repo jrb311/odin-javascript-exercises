450f26ea4202e49f832ee9d486a0580d60103c09
jrb311 <159147700+jrb311@users.noreply.github.com>
2024-03-09 10:03:43 -0500

    Complete exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let i = 0;
+    let reverseString = '';
+    for (let l = (string.length - 1); l >= 0; l--)
+    {
+        reverseString += string.charAt(l);
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
