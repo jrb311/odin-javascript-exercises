a12a5093c9b58902f16ca6b7696b1aa5b56a3916
jrb311 <159147700+jrb311@users.noreply.github.com>
2024-03-09 11:59:17 -0500

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (let i = args.length - 1; i >= 0; i-- )
+    {
+        while (array.includes(args[i]))
+        {
+            array.splice(array.indexOf(args[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
