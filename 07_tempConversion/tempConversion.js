e8046276ae3b2236eaff90e6f7be10ff9f74f19c
jrb311 <159147700+jrb311@users.noreply.github.com>
2024-03-09 13:58:57 -0500

    Complete exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let celsius = (degree - 32) * (5/9);
+  return Math.round(celsius * 10)/10;
+ 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let faren = (degree * (9/5) + 32);
+  return Math.round(faren * 10)/10;
 };
 
 // Do not edit below this line
