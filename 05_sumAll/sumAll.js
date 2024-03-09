78da21be262acd5cd009bf0ecec1a6a8aca63e76
jrb311 <159147700+jrb311@users.noreply.github.com>
2024-03-09 12:37:33 -0500

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,32 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    if (numOne < 0 || numTwo < 0 || typeof(numOne) !== 'number' || typeof(numTwo) !== 'number')
+    {
+        return 'ERROR';
+    }
+
+    let sum = 0;
+
+    if(numOne > numTwo)
+    {
+        for (; numTwo <= numOne; numTwo++)
+        {
+            sum += numTwo;
+        }
+        return sum;
+    }
+    else if (numTwo > numOne)
+    {
+        for (; numOne <= numTwo; numOne++)
+        {
+            sum += numOne;
+        }
+        return sum;
+    }
+    else
+    {
+        sum = numOne + numTwo;
+        return sum;
+    }
 
 };
 
